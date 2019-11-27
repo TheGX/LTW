4eff6d4e33577092541bb43fb0afe41287ab669f
Nuno Gomes <32264676+TheNunoGomes@users.noreply.github.com>
2019-11-27 11:09:43 +0000

    newest version of database
    removed user type and added some attributes to reservations

--- database/database.sql
+++ database/database.sql
@@ -1,14 +1,13 @@
 CREATE TABLE Users (
-    ID              INTEGER PRIMARY KEY,
-    Username        TEXT    NOT NULL    UNIQUE, 
-    Password        TEXT    NOT NULL,
-    Name            TEXT    NOT NULL,
-    Email           TEXT    NOT NULL    UNIQUE,
-    UserType        INTEGER REFERENCES  UserTypes,
-    Accomodation    INTEGER REFERENCES  Reservation,
-    Biography       TEXT,
-    PhoneNumber     TEXT,
-    Avatar          TEXT
+    ID                  INTEGER PRIMARY KEY,
+    Username            TEXT    NOT NULL        UNIQUE, 
+    Password            TEXT    NOT NULL,
+    Name                TEXT    NOT NULL,
+    Email               TEXT    NOT NULL        UNIQUE,
+    ReservationID       INTEGER REFERENCES      Reservation,
+    Biography           TEXT,
+    PhoneNumber         TEXT,
+    Avatar              TEXT
 );
 
 CREATE TABLE Houses ( 
@@ -25,17 +24,16 @@ CREATE TABLE Reservation (
     ID          INTEGER PRIMARY KEY,
     StartDate   DATE    NOT NULL,
     EndDate     DATE    NOT NULL,
-    Guest       INTEGER NOT NULL    REFERENCES  Users
+    GuestID     INTEGER NOT NULL    REFERENCES  Users,
+    HouseID     INTEGER NOT NULL    REFERENCES  Houses,
+    PricePaid   DECIMAL NOT NULL,
+    Comment     TEXT,
+    CommentDate DATETIME,
+    Reply       TEXT,
+    ReplyDate   DATETIME,
+    HouseRating INTEGER,
+    GuestRating INTEGER
 );
-
-CREATE TABLE UserTypes (
-    ID      INTEGER PRIMARY KEY,
-    Name    TEXT    NOT NULL    UNIQUE
-);
-
-INSERT INTO UserTypes VALUES (NULL, 'ADMIN');
-INSERT INTO UserTypes VALUES (NULL, 'TOURIST');
-INSERT INTO UserTypes VALUES (NULL, 'OWNER');
 
 INSERT INTO Users VALUES (
     NULL, --ID
@@ -43,7 +41,6 @@ INSERT INTO Users VALUES (
     'q46r&@NwvJ&yC2$^$A3d=9+F9MR!bL7ebt!?Jxdzm=Gy@AwGFpzH?CZ-g_6NL$X=b!*A$=+xK$=k+&Pu!+vuB6BJz5k8yCn6jd=RsCQtJuJj3g72q5uBA-KKc#ZaYgMx', --Password
     'System Admin', --Name
     'sadmin@rentals.com', --email
-    'ADMIN', --User Type
     NULL, --Accomodation
     NULL, --Biography
     NULL, --Phone
