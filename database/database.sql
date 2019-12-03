e71025110bcbd1dc4fd4e507ce4eae422b0c51db
Nuno Gomes <32264676+TheNunoGomes@users.noreply.github.com>
2019-12-03 18:00:51 +0000

    yet another update on the database
    added more attributes and chat support

--- database/database.sql
+++ database/database.sql
@@ -4,7 +4,9 @@ CREATE TABLE Users (
     Password            TEXT    NOT NULL,
     Name                TEXT    NOT NULL,
     Email               TEXT    NOT NULL        UNIQUE,
-    ReservationID       INTEGER REFERENCES      Reservation,
+    Address             TEXT,
+    LanguagesSpoken     TEXT,
+    Profession          TEXT,
     Biography           TEXT,
     PhoneNumber         TEXT,
     Avatar              TEXT
@@ -12,20 +14,36 @@ CREATE TABLE Users (
 
 CREATE TABLE Houses ( 
     ID              INTEGER PRIMARY KEY,
-    Name            TEXT    NOT NULL,
-    Address         TEXT    NOT NULL,
+    OwnerID         INTEGER NOT NULL    REFERENCES Users,
     Title           TEXT    NOT NULL,
+    Country         TEXT    NOT NULL,
+    City            TEXT    NOT NULL,
+    Street          TEXT    NOT NULL,
+    ZIPCode         TEXT    NOT NULL,
+    Thumbnail       TEXT    NOT NULL,
+    Picture1        TEXT,
+    Picture2        TEXT,
+    Picture3        TEXT,
+    Picture4        TEXT,
+    Picture5        TEXT,
+    Picture6        TEXT,
+    Picture7        TEXT,
+    Picture8        TEXT,
+    Picture9        TEXT,
+    Bathrooms       INTEGER,
+    SingleBeds      INTEGER,
+    DoubleBeds      INTEGER,
     Description     TEXT,
     DailyCost       DECIMAL,
-    OwnerID         INTEGER NOT NULL    REFERENCES Users
+    HouseType       TEXT
 );
 
 CREATE TABLE Reservation (
     ID          INTEGER PRIMARY KEY,
-    StartDate   DATE    NOT NULL,
-    EndDate     DATE    NOT NULL,
     GuestID     INTEGER NOT NULL    REFERENCES  Users,
     HouseID     INTEGER NOT NULL    REFERENCES  Houses,
+    StartDate   DATE    NOT NULL,
+    EndDate     DATE    NOT NULL,
     PricePaid   DECIMAL NOT NULL,
     Comment     TEXT,
     CommentDate DATETIME,
@@ -35,14 +53,26 @@ CREATE TABLE Reservation (
     GuestRating INTEGER
 );
 
+CREATE TABLE Chat (
+  ID            INTEGER     PRIMARY KEY,
+  SenderID      INTEGER     NOT NULL    REFERENCES Users,
+  ReceiverID    INTEGER     NOT NULL    REFERENCES Users,
+  Date          DATETIME    NOT NULL,
+  Text          TEXT        NOT NULL
+);
+
+
 INSERT INTO Users VALUES (
     NULL, --ID
     'systemadmin', --username
     'q46r&@NwvJ&yC2$^$A3d=9+F9MR!bL7ebt!?Jxdzm=Gy@AwGFpzH?CZ-g_6NL$X=b!*A$=+xK$=k+&Pu!+vuB6BJz5k8yCn6jd=RsCQtJuJj3g72q5uBA-KKc#ZaYgMx', --Password
     'System Admin', --Name
     'sadmin@rentals.com', --email
-    NULL, --Accomodation
+    NULL,
+    NULL,
+    NULL,
     NULL, --Biography
     NULL, --Phone
     NULL  --Picture
+    
 );
