d73e065db2e7a924e53f2f3eca34a771b9e24fa1
GoncaloXavier <goncalox11@gmail.com>
2019-12-06 14:24:29 +0000

    Added database to the tag

---
+++ database/reservations.php
@@ -0,0 +1,39 @@
+<?php
+    //Create a new reservation for $guest in $house from $start to $end with a $daily cost (WORKING)
+    function createReservation($guest, $house, $start, $end, $dailyCost) {
+        global $conn;
+
+        $stmt = $conn->prepare('INSERT INTO Reservation VALUES(NULL, ?, ?, ?, ?, ?, NULL, NULL, NULL, NULL, NULL, NULL)');
+        $stmt->execute([$guest, $house, $start, $end, $dailyCost]);
+
+        return $stmt->fetch();
+    }
+    //Retrieve all Reservations (WORKING)
+    function getAllReservations() {
+        global $conn;
+
+        $stmt = $conn->prepare('SELECT * FROM Reservation');
+        $stmt->execute();
+
+        return $stmt->fetchAll();
+    }
+    //retrieve reservations from $guest
+    function getReservationsFromUser($guest) {
+        global $conn;
+
+        //CODE HERE
+        
+        $stmt = $conn->prepare();
+        return $stmt->fetchAll();
+    }
+    //retrieve reservations in $house
+    function getReservationsInHouse($house) {
+        global $conn;
+
+        //CODE HERE
+
+        $stmt = $conn->prepare();
+        return $stmt->fetchAll();
+    }
+
+?>
---
+++ database/users.php
@@ -0,0 +1,49 @@
+<?php
+    // Adds user to the Database (WORKING)
+    function createUser($username, $password, $name, $email) {
+        global $conn;
+
+        $stmt = $conn->prepare('INSERT INTO Users VALUES(NULL, ?, ?, ?, ?, NULL, NULL, NULL, NULL, NULL, NULL)');
+        $stmt->execute([$username, $password, $name, $email]);
+
+        return $stmt->fetch();  
+    }
+
+    // Queries users from the names and usernames in the database (WORKING)
+    function searchUsers($query) {
+        global $conn;
+
+        $stmt = $conn->prepare('SELECT ID, Username, Name, Email FROM Users WHERE Name LIKE ? OR Username LIKE ?');
+        $stmt->execute(["%$query%", "%$query%"]);
+
+        return $stmt->fetchAll();
+    }
+
+    // Queries user info of an ID in the Database (WORKING)
+    function getInfoFromID($ID) {
+        global $conn;
+
+        $stmt = $conn->prepare('SELECT * FROM Users WHERE ID = ?');
+        $stmt->execute(array($ID));
+
+        return $stmt->fetch();
+    }
+
+    // Queries user info of a username in the Database (WORKING)
+    function getInfoFromUsername($username) {
+        global $conn;
+
+        $stmt = $conn->prepare('SELECT * FROM Users WHERE Username = ?');
+        $stmt->execute(array($username));
+        return $stmt->fetch();
+    }
+
+    // Queries user info from email in the database (WORKING)
+    function getInfoFromEmail($email) {
+        global $conn;
+
+        $stmt = $conn->prepare('SELECT * FROM Users WHERE Email = ?');
+        $stmt->execute(array($email));
+        return $stmt->fetch();
+    }
+?>
