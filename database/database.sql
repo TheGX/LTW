38156721bbc2750594e3fa28578ef5bcbb815676
GoncaloXavier <goncalox11@gmail.com>
2019-12-06 11:26:32 +0000

    Added database folder from Nuno

---
+++ database/database.sql
@@ -0,0 +1,77 @@
+CREATE TABLE Users (
+    ID                  INTEGER PRIMARY KEY,
+    Username            TEXT    NOT NULL        UNIQUE, 
+    Password            TEXT    NOT NULL,
+    Name                TEXT    NOT NULL,
+    Email               TEXT    NOT NULL        UNIQUE,
+    Address             TEXT,
+    LanguagesSpoken     TEXT,
+    Profession          TEXT,
+    Biography           TEXT,
+    PhoneNumber         TEXT,
+    Avatar              TEXT
+);
+
+CREATE TABLE Houses ( 
+    ID              INTEGER PRIMARY KEY,
+    OwnerID         INTEGER NOT NULL    REFERENCES Users,
+    Title           TEXT    NOT NULL,
+    Country         TEXT    NOT NULL,
+    City            TEXT    NOT NULL,
+    Street          TEXT    NOT NULL,
+    ZIPCode         TEXT    NOT NULL,
+    Thumbnail       TEXT    NOT NULL,
+    DailyCost       DECIMAL NOT NULL,
+    Picture1        TEXT,
+    Picture2        TEXT,
+    Picture3        TEXT,
+    Picture4        TEXT,
+    Picture5        TEXT,
+    Picture6        TEXT,
+    Picture7        TEXT,
+    Picture8        TEXT,
+    Picture9        TEXT,
+    Bathrooms       INTEGER,
+    SingleBeds      INTEGER,
+    DoubleBeds      INTEGER,
+    Description     TEXT,
+    HouseType       TEXT
+);
+
+CREATE TABLE Reservation (
+    ID          INTEGER PRIMARY KEY,
+    GuestID     INTEGER NOT NULL    REFERENCES  Users,
+    HouseID     INTEGER NOT NULL    REFERENCES  Houses,
+    StartDate   DATE    NOT NULL,
+    EndDate     DATE    NOT NULL,
+    PricePaid   DECIMAL NOT NULL,
+    Comment     TEXT,
+    CommentDate DATETIME,
+    Reply       TEXT,
+    ReplyDate   DATETIME,
+    HouseRating INTEGER,
+    GuestRating INTEGER
+);
+
+CREATE TABLE Chat (
+  ID            INTEGER     PRIMARY KEY,
+  SenderID      INTEGER     NOT NULL    REFERENCES Users,
+  ReceiverID    INTEGER     NOT NULL    REFERENCES Users,
+  Date          DATETIME    NOT NULL,
+  Text          TEXT        NOT NULL
+);
+
+
+INSERT INTO Users VALUES (
+    NULL, --ID
+    'systemadmin', --username
+    'q46r&@NwvJ&yC2$^$A3d=9+F9MR!bL7ebt!?Jxdzm=Gy@AwGFpzH?CZ-g_6NL$X=b!*A$=+xK$=k+&Pu!+vuB6BJz5k8yCn6jd=RsCQtJuJj3g72q5uBA-KKc#ZaYgMx', --Password
+    'System Admin', --Name
+    'sadmin@rentals.com', --email
+    NULL,
+    NULL,
+    NULL,
+    NULL, --Biography
+    NULL, --Phone
+    NULL  --Picture
+);
---
+++ database/test.php
@@ -0,0 +1,50 @@
+<?php
+  session_start(['cookie_httponly' => true]);
+  
+  $conn = new PDO('sqlite:database.db');
+  $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); 
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
+<h1>RUNNING PHP</h1>
+
+<?php
+    include_once('houses.php');
+    include_once('users.php');
+    include_once('reservations.php');
+
+
+?>
+    <h1>
+        <?php
+            //createUser($username, $password, $name, $email);
+            $houseID = 1;
+            //createReservation(getInfoFromUsername("nunogomes")['ID'], $houseID, "12-6-2019", "12-7-2019", getRentPrice($houseID))
+        ?>
+    </h1>
+
+
+<?php
+    // $address['Country'] = "London";
+    // $address['City'] = "Porto";
+    // $address['Street'] = "Cedofeita";
+    // $address['ZIPCode'] = "42069";
+
+    //createHouse(getInfoFromUsername("marquerere")['ID'], "big house", json_encode($address), "housebig.jpg", 420);
+    foreach(getAllHouses() as $house) {
+?>
+        <h1>Title: <?php echo($house['DailyCost'])?></h1>
+<?php 
+    }
+
+    foreach(getAllReservations() as $reservation) {
+?>
+        <h1>Price Paid: <?php echo($reservation['PricePaid'])?></h1>
+<?php 
+    }
+
+    foreach(getHousesFromOwner(getInfoFromUsername("nunogomes")['ID']) as $house) {
+?>
+        <h1>House: <?php echo($house['Title'])?></h1>
+<?php
+    }
+?>
