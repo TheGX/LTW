1f37fb58adf2e3a08e131ba19704d4c901f302a6
GoncaloXavier <goncalox11@gmail.com>
2019-12-11 00:34:56 +0000

    Added more actions from edit profile

--- pages/editprofile.php
+++ pages/editprofile.php
@@ -12,10 +12,10 @@
     <section id="content">
         <section id="sideInfo">
             <img src="pictures/bigprofilepic.png" alt="Profile Picture">
-            <form action="upload.php" method="post" enctype="multipart/form-data">
-            Upload new photo:
-            <input type="file" name="fileToUpload" id="fileToUpload">
-            <input type="submit" value="Upload Image" name="submit">
+            <form action="../actions/action_uploadImage.php" method="post" enctype="multipart/form-data">
+                Upload new photo:
+                <input type="file" name="fileToUpload" id="fileToUpload">
+                <input type="submit" value="Upload Image" name="submit">
             </form>
         </section>
         <section id="EditInfo">
@@ -23,36 +23,44 @@
                 <h3>Edit Profile</h3>
             </header>
             <h4>Password:</h4>
-            <label for="pass">Old Password:</label>
-            <input type="password" id="pass" name="password">
-                
-            <label for="newpass">New Password:</label>
-            <input type="password" id="newpass" name="newpassword">
-            <input type="submit" value="Change Password">
+            <form action="../actions/action_editPassword.php" method='post'>
+                <label for="pass">Old Password:</label>
+                <input type="password" id="pass" name="password">    
+                <label for="newpass">New Password:</label>
+                <input type="password" id="newpass" name="newpassword">
+                <input type="submit" value="Change Password">
+            </form>
 
             <h4>Your Name:</h4>
-            <label for="newuser">Username:</label>
-            <input type="text" id="newuser" name="username">
-            <input type="submit" value="Change Username">
-            <h4></h4>
-            <label for="newname">Legal Name:</label>
-            <input type="text" id="newname" name="legalname">
-            <input type="submit" value="Change Legal Name">
+            <form action="../actions/action_editUsername.php" method='post'>
+                <label for="newuser">Username:</label>
+                <input type="text" id="newuser" name="username">
+                <input type="submit" value="Change Username">
+            </form>
+            <form action="../actions/action_editName.php">
+                <label for="newname">Name:</label>
+                <input type="text" id="newname" name="name">
+                <input type="submit" value="Change Legal Name">
+            </form>
 
+            <h4>Your Email:</h4>
+            <form action="../actions/action_editEmail.php" method='post'>
+                <label for="newEmail">Email:</label>
+                <input type="text" id="newEmail" name="email">
+                <input type="submit" value="Change Email">
+            </form>
+            
             <h4>Biography:</h4>
-            <textarea name="text" rows="5" cols="100">...</textarea>
-            <br>
-            <input type="submit" value="Change Biography">
+            <form action="../actions/action_editBio.php" method='post'>
+                <textarea name="text" rows="5" cols="100">...</textarea><br>
+                <input type="submit" value="Change Biography">
+            </form>
 
             <h4>Phone Number</h4>
-            <input type="number" id="phone" name="phonenumber">
-            <input type="submit" value="Change Phone Number">
-
-            <h4>Save Profile</h4>
-            <form action="../actions/action_editProfile.php">
-            <input type="submit" value="Submit">
+            <form action="../actions/action_editPhoneNumber.php" method='post'>
+                <input type="number" id="phone" name="phonenumber">
+                <input type="submit" value="Change Phone Number">
             </form>
-    </section>
-    
+        </section>
     </section>
 
